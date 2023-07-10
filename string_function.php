9eda00436e41d043fb3b3ea20333e81ef01ec8d4
chetanapatil23 <137033857+chetanapatil23@users.noreply.github.com>
2023-07-10 14:27:59 +0530

    Add files via upload

---
+++ string_function.php
@@ -0,0 +1,129 @@
+<?php
+echo "SHIVA";
+echo"<br>";
+$a= "MAHADEV";
+echo $a;
+echo"<br>";
+$str1="Hello world!";
+$str2="What a nice day!";
+echo $str1 . " " . $str2;
+echo"<br>";
+echo strlen("CHETANA");
+echo"<br>";
+echo str_word_count("Patil chetana");
+echo"<br>";
+echo strrev("welcome");
+echo"<br>";
+echo strpos("Hello how are you..!", "are");
+echo "<br>";
+echo str_replace("world", "Dolly", "Hello world!");
+echo"<br>";
+print "Disha";
+echo"<br>";
+echo strtoupper("chetana");
+echo"<br>";
+echo strtolower("CHETANA");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
